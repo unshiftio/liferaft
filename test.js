b09069125551277fe44e38df2e9b999845fc78f7
Arnout Kazemier <info@3rd-Eden.com>
2014-10-30 09:06:54 +0100

    [test] Wrap everything in the `raft consens algorithm`

--- test.js
+++ test.js
@@ -392,105 +392,43 @@ describe('liferaft', function () {
   // The following set of tests asserts if we correctly follow the Raft white
   // paper and that our module does what is expected of it.
   //
-  describe('election', function () {
-    describe('vote', function () {
-      it('ignores stale votes when term is out of date', function () {
-        raft.once('vote', function (packet, accepted) {
-          throw new Error('Broken');
-        });
-
-        raft.change({ term: 139 });
-        raft.emit('data', {
-          name: 'vladimir',
-          type: 'vote',
-          term: 138
-        });
-      });
-
-      it('votes on first come first serve basis', function (next) {
-        raft.once('vote', function (packet, accepted) {
-          assume(accepted).is.true();
-
+  describe('Raft Consensus Algorithm', function () {
+    describe('election', function () {
+      describe('vote', function () {
+        it('ignores stale votes when term is out of date', function () {
           raft.once('vote', function (packet, accepted) {
-            assume(accepted).is.false();
-            next();
+            throw new Error('Broken');
+          });
+
+          raft.change({ term: 139 });
+          raft.emit('data', {
+            name: 'vladimir',
+            type: 'vote',
+            term: 138
           });
         });
 
-        raft.change({ term: 139 });
-        raft.emit('data', { name: 'vladimir', term: raft.term, type: 'vote' });
-        raft.emit('data', { name: 'anatoly', term: raft.term, type: 'vote' });
-      });
-    });
+        it('votes on first come first serve basis', function (next) {
+          raft.once('vote', function (packet, accepted) {
+            assume(accepted).is.true();
 
-    describe('voted', function () {
-      it('only increments votes when being a CANDIDATE', function () {
-        assume(raft.votes.granted).equals(0);
-        assume(raft.state).equals(Raft.FOLLOWER);
+            raft.once('vote', function (packet, accepted) {
+              assume(accepted).is.false();
+              next();
+            });
+          });
 
-        raft.emit('data', {
-          data: { granted: true },
-          term: raft.term,
-          state: Raft.FOLLOWER,
-          name: 'foobar',
-          type: 'voted'
+          raft.change({ term: 139 });
+          raft.emit('data', { name: 'vladimir', term: raft.term, type: 'vote' });
+          raft.emit('data', { name: 'anatoly', term: raft.term, type: 'vote' });
         });
-
-        assume(raft.votes.granted).equals(0);
-
-        //
-        // Promote our selfs to candidate.
-        //
-        raft.promote();
-        assume(raft.state).equals(Raft.CANDIDATE);
-        assume(raft.votes.granted).equals(1);
-
-        raft.emit('data', {
-          data: { granted: true },
-          term: raft.term,
-          state: Raft.FOLLOWER,
-          name: 'foobar',
-          type: 'voted'
-        });
-
-        assume(raft.votes.granted).equals(2);
-        assume(raft.state).equals(Raft.CANDIDATE);
       });
 
-      it('only accepts granted votes', function () {
-        raft.promote();
-        assume(raft.state).equals(Raft.CANDIDATE);
-        assume(raft.votes.granted).equals(1);
+      describe('voted', function () {
+        it('only increments votes when being a CANDIDATE', function () {
+          assume(raft.votes.granted).equals(0);
+          assume(raft.state).equals(Raft.FOLLOWER);
 
-        raft.emit('data', {
-          data: { granted: false },
-          term: raft.term,
-          state: Raft.FOLLOWER,
-          name: 'foobar',
-          type: 'voted'
-        });
-
-        assume(raft.votes.granted).equals(1);
-        assume(raft.state).equals(Raft.CANDIDATE);
-      });
-
-      it('changes to leader when majority has voted', function (next) {
-        //
-        // Feed raft some "nodes" so it can actually reach a consensus when it
-        // received a majority of the votes.
-        //
-        raft.nodes.push(1, 2, 3, 4, 5);
-        raft.promote();
-
-        raft.once('state change', function (currently, previously) {
-          assume(previously).equals(Raft.CANDIDATE);
-          assume(raft.state).equals(Raft.LEADER);
-          assume(currently).equals(Raft.LEADER);
-
-          next();
-        });
-
-        for (var i = 0; i < 4; i++) {
           raft.emit('data', {
             data: { granted: true },
             term: raft.term,
@@ -498,9 +436,73 @@ describe('liferaft', function () {
             name: 'foobar',
             type: 'voted'
           });
-        }
 
-        assume(raft.state).equals(Raft.LEADER);
+          assume(raft.votes.granted).equals(0);
+
+          //
+          // Promote our selfs to candidate.
+          //
+          raft.promote();
+          assume(raft.state).equals(Raft.CANDIDATE);
+          assume(raft.votes.granted).equals(1);
+
+          raft.emit('data', {
+            data: { granted: true },
+            term: raft.term,
+            state: Raft.FOLLOWER,
+            name: 'foobar',
+            type: 'voted'
+          });
+
+          assume(raft.votes.granted).equals(2);
+          assume(raft.state).equals(Raft.CANDIDATE);
+        });
+
+        it('only accepts granted votes', function () {
+          raft.promote();
+          assume(raft.state).equals(Raft.CANDIDATE);
+          assume(raft.votes.granted).equals(1);
+
+          raft.emit('data', {
+            data: { granted: false },
+            term: raft.term,
+            state: Raft.FOLLOWER,
+            name: 'foobar',
+            type: 'voted'
+          });
+
+          assume(raft.votes.granted).equals(1);
+          assume(raft.state).equals(Raft.CANDIDATE);
+        });
+
+        it('changes to leader when majority has voted', function (next) {
+          //
+          // Feed raft some "nodes" so it can actually reach a consensus when it
+          // received a majority of the votes.
+          //
+          raft.nodes.push(1, 2, 3, 4, 5);
+          raft.promote();
+
+          raft.once('state change', function (currently, previously) {
+            assume(previously).equals(Raft.CANDIDATE);
+            assume(raft.state).equals(Raft.LEADER);
+            assume(currently).equals(Raft.LEADER);
+
+            next();
+          });
+
+          for (var i = 0; i < 4; i++) {
+            raft.emit('data', {
+              data: { granted: true },
+              term: raft.term,
+              state: Raft.FOLLOWER,
+              name: 'foobar',
+              type: 'voted'
+            });
+          }
+
+          assume(raft.state).equals(Raft.LEADER);
+        });
       });
     });
   });
